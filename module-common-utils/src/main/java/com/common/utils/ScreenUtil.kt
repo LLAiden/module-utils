d98dab401cef4f3884119a8cfe2b7292919dd29f
LLAiden <LLAiden@qq.com>
2023-05-19 17:56:21 +0800

    添加扩张函数

--- module-common-utils/src/main/java/com/common/utils/ScreenUtil.kt
+++ module-common-utils/src/main/java/com/common/utils/ScreenUtil.kt
@@ -1,7 +1,9 @@
 package com.common.utils
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.res.Resources
+import com.common.utils.container.ApplicationContainer
 
 object ScreenUtil {
     fun getScreenWidth(context: Context): Int {
@@ -19,5 +21,24 @@ object ScreenUtil {
 
     fun px2dp(pxValue: Int): Float {
         return pxValue / Resources.getSystem().displayMetrics.density
+    }
+
+    @SuppressLint("InternalInsetResource")
+    fun getStatusBarHeight(context: Context): Int {
+        val resourceId = context.resources.getIdentifier("status_bar_height", "dimen", "android")
+        return context.resources.getDimensionPixelSize(resourceId)
+    }
+
+    @SuppressLint("InternalInsetResource", "DiscouragedApi")
+    fun getStatusBarHeight(): Int {
+        val application = ApplicationContainer.getApp()
+        val resourceId: Int = application.resources
+            .getIdentifier("status_bar_height", "dimen", "android")
+        return application.resources.getDimensionPixelSize(resourceId)
+    }
+
+    fun sp2px(spValue: Float): Int {
+        val fontScale: Float = Resources.getSystem().displayMetrics.scaledDensity
+        return (spValue * fontScale + 0.5f).toInt()
     }
 }
--- module-common-utils/src/main/java/com/common/utils/extend/CtxExt.kt
+++ module-common-utils/src/main/java/com/common/utils/extend/CtxExt.kt
@@ -1,6 +1,9 @@
 package com.common.utils.extend
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
+import android.os.Bundle
 import androidx.lifecycle.*
 
 fun Context.getViewModelProvider(): ViewModelStoreOwner? {
@@ -19,4 +22,13 @@ fun Context.getLifecycleOwner(): LifecycleOwner? {
 
 fun Context.getLifecycleScope(): LifecycleCoroutineScope? {
     return getLifecycleOwner()?.lifecycleScope
+}
+
+fun Context.goPage(kClass: Class<out Activity>, bundle: Bundle? = null) {
+    Intent(this, kClass).apply {
+        bundle?.let {
+            putExtras(bundle)
+        }
+        startActivity(this)
+    }
 }
