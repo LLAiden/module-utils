d5d59bdae83c24887c7b5ecb587d6ca390946d66
llaiden <32894618+LLAiden@users.noreply.github.com>
2024-03-12 17:44:31 +0800

    Update ClickUtil.kt

--- module-common-utils/src/main/java/com/common/utils/ClickUtil.kt
+++ module-common-utils/src/main/java/com/common/utils/ClickUtil.kt
@@ -5,27 +5,18 @@ import android.view.View
 
 object ClickUtil {
 
-    private const val INTERVAL = 500L
+   private const val DEFAULT_INTERVAL_MS = 500L
 
-    fun isDoubleClick(view: View): Boolean {
-        return isDoubleClick(view, INTERVAL)
-    }
+    fun isDoubleClick(view: View, customIntervalMs: Long = DEFAULT_INTERVAL_MS): Boolean {
+        val lastClickTimestampKey = TagUtil.generateTagId(Constants.TAG.TAG_DOUBLE_CLICK)
+        val lastClickTimestamp = view.getTag(lastClickTimestampKey) as? Long ?: 0L
+        val currentTime = System.currentTimeMillis()
 
-    fun isDoubleClick(view: View, interval: Long): Boolean {
-        var result = false
-        val generateTagId = TagUtil.generateTagId(Constants.TAG.TAG_DOUBLE_CLICK)
-        Log.e("ClickUtil", "isDoubleClick: generateTagId: $generateTagId")
-        val tag = view.getTag(generateTagId)
-        val currentTimestamp = currentTimestamp
-        if (tag is Long) {
-            val pastTime = currentTimestamp - tag
-            if (pastTime <= interval) {
-                result = true
-            }
+        val isDoubleClick = currentTime - lastClickTimestamp <= customIntervalMs
+        if (!isDoubleClick) {
+            // Only update the tag if it's not a double click
+            view.setTag(lastClickTimestampKey, currentTime)
         }
-        view.setTag(generateTagId, currentTimestamp)
-        return result
+        return isDoubleClick
     }
-
-    private val currentTimestamp: Long get() = System.currentTimeMillis()
 }
