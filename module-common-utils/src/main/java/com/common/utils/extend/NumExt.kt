b5d8ec3df84b69ca5b76868cbbca6e5706826d57
LLAiden <LLAiden@qq.com>
2024-04-12 11:37:46 +0800

    修改方法名

--- module-common-utils/src/main/java/com/common/utils/extend/NumExt.kt
+++ module-common-utils/src/main/java/com/common/utils/extend/NumExt.kt
@@ -11,9 +11,9 @@ val Int.dp get() = ScreenUtil.dp2px(this.toFloat())
 
 val Int.fdp get() = ScreenUtil.dp2px(this.toFloat()).toFloat()
 val Float.dp get() = ScreenUtil.dp2px(this)
-fun Int.colorAlpha(@IntRange(from = 1, to = 100) value: Int) = ColorUtil.alphaColor(this, value)
+fun Int.alpha(@IntRange(from = 1, to = 100) value: Int) = ColorUtil.alphaColor(this, value)
 
-fun Int.compatibleRTL(size: Int, isRTL: Boolean = ScreenUtil.isRTL()): Int {
+fun Int.rtl(size: Int, isRTL: Boolean = ScreenUtil.isRTL()): Int {
     return if (isRTL) {
         size - 1 - this
     } else this
